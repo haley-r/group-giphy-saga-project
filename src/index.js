5726a6880209a33ccff507e023f90f2a3519b54c
Mulki Ali <mulki.ali@hotmail.fi>
2020-03-05 13:30:30 -0600

    changes to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
+
+
+
+function* rootSaga(){
+yield takeEvery ('FETCH_GIF', fetchGifs)
+}
+
+function* fetchGifs(){
+    const gifDisplay = yield axios.get('/')
+}
+
+const gifReducer = (state=[], action) =>{
+    switch(action.type){
+        case 'GET_GIF':
+            return action.payload;
+        default: 
+            return state
+    }
+    
+}
+
+
+const sagaMiddleware = createSagaMiddleware();
+const storeInstance = createStore(
+    combineReducers({
+        gifReducer
+    }),
+
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('react-root'));
