21773e6f241f99608c43c6893b16c6d57b1ac4ef
haley-r <haleyryan77@gmail.com>
2020-03-05 12:23:06 -0600

    hardcoded search works in search router

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 class Search extends Component {
 
+  state = {
+    searchInput: ''
+  }
+
+  search=()=> {
+    let searchString=this.state.searchInput;
+    console.log(`gonna search for ${searchString}`);
+
+    axios({
+      method: 'GET',
+      url: '/api/search'
+    }).then((response)=> {
+      console.log('this is the client side: ', response.data.data)
+    }).catch((error) => {
+      console.log(error);
+    })//end axios
+  }
+
+  trackSearchInput=(event)=>{
+    this.setState({
+      searchInput: event.target.value
+    })
+  }
+
   render() {
     return (
-      <div>
+      <div className="Search">
+        <input type="text" onChange={this.trackSearchInput} placeholder="what would you like to see?" value={this.state.searchInput}/>
+        <button onClick={this.search}>send search</button>
      
       </div>
     );
