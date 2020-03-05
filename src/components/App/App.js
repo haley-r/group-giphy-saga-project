0ae3c427ec01fbe0489f6f996f64121b7a7de938
Mulki Ali <mulki.ali@hotmail.fi>
2020-03-05 11:08:14 -0600

    components set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import Search from "../Search/Search";
+import Favorites from "../Favorites/Favorites";
 
 
 class App extends Component {
 
   render() {
     return (
+      <Router>
       <div>
         <h1>Giphy Search!</h1>
+        <ul>
+          <li><Link to="/">Search!</Link></li>
+          <li><Link to="/favorites">Favorites!</Link></li>
+        </ul>
+        <Route exact path='/' component={Search} />
+        <Route path='/favorites' component={Favorites} />
       </div>
+     </Router>
+
     );
   }
   
---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Favorites extends Component {
+
+  render() {
+    return (
+      <div>
+      <h1>Favorites!</h1>
+      </div>
+    );
+  }
+  
+}
+
+export default Favorites;
