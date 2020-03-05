6ddb1fccdbc0be1e29ed3bf47b8b61d058a1b86d
Melinda Nguyen <melindanguyenn@gmail.com>
2020-03-05 13:39:18 -0600

    call gif in image tag to render

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Search extends Component {
   componentDidMount() {
-  this.showGIF()
+    this.showGIF();
   }
-  
+
   showGIF() {
     this.props.dispatch({
       type: "GET_GIF"
-    })
+    });
   }
+  
   render() {
     return (
       <div>
-     
+        <img alt="gif" src={this.props.reduxState.gifReducer}></img>
       </div>
     );
   }
-  
 }
-const getStore = (reduxState) => ({
+const getStore = reduxState => ({
   reduxState
 });
 export default connect(getStore)(Search);
