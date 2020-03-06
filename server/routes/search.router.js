1f37d6d911152fe13de19122d7892e257570e444
Melinda Nguyen <melindanguyenn@gmail.com>
2020-03-06 10:22:25 -0600

    like button sends link to DB

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -1,18 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 
+router.get("/:search", (req, res) => {
+  console.log("this is req.params", req.params);
 
-router.get('/:search', (req, res) => {
-    console.log('this is req.params', req.params);
-    
-    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&limit=5&q=${req.params.search}`)
-        .then((response) => {
-            console.log('this is the API response', response.data);
-            res.send(response.data);
-        }).catch((error) => {
-            console.log(error);
-        })
-})
+  axios
+    .get(
+      `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&limit=5&q=${req.params.search}`
+    )
+    .then(response => {
+      console.log("this is the API response", response.data);
+      res.send(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 module.exports = router;
